b62a6812ae732ba9bdc2d171b34b68e232570e4c
Daniel Davis <daniel@hsdesignhouse.com>
2014-01-30 11:28:57 -0600

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,15 +7,21 @@ describe User do
                      password: "foobar", password_confirmation: "foobar")
   end
 
+  it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
+
 	subject { @user }
   	
   	it { should respond_to(:name) }
   	it { should respond_to(:email) }
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
-    it { should respond_to(:password_confirmation) }
-
+    
+    
   	it { should be_valid }
+
+
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -96,4 +102,9 @@ describe "return value of authenticate method" do
     specify { expect(user_for_invalid_password).to be_false }
   end
 end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
